4c2936a42a9e02f5f9cdebe5307e2cd7fce3b42b
Omar Salah <omar.salah1597@gmail.com>
2025-10-15 09:59:08 +0300

    feat: implement request and response caching for ephemeral exchanges routes

--- src/utils/cache.ts
+++ src/utils/cache.ts
@@ -3,33 +3,22 @@ import { LruCache } from '@digitalcredentials/lru-memoize'
 import { VC_API_EXCHANGE_TIMEOUT } from '../config.default'
 
 declare global {
-  var exchanges: any | undefined
-}
-
-export async function pollExchange({
-  exchangeUrl,
-  onFetchVP,
-  stopPolling
-}: {
-  exchangeUrl: string
-  onFetchVP: (vp: any) => void
-  stopPolling: () => void
-}): Promise<void> {
-  const result = await fetch(exchangeUrl, {})
-
-  if (result.ok && result.status === 200) {
-    const vp = (await result.json()) as any
-    console.log('Fetched vp:', typeof vp, vp)
-    onFetchVP(vp)
-    stopPolling()
-  }
+  var rpRequestCache: any | undefined
+  var rpResponseCache: any | undefined
 }
 
 // identical shape as V+, just using LruCache
-export const exchanges =
-  globalThis.exchanges ||
+export const rpRequestCache =
+  globalThis.rpRequestCache ||
   new LruCache({
     maxAge: VC_API_EXCHANGE_TIMEOUT as number
   })
 
-globalThis.exchanges = exchanges
+export const rpResponseCache =
+  globalThis.rpResponseCache ||
+  new LruCache({
+    maxAge: VC_API_EXCHANGE_TIMEOUT as number
+  })
+
+globalThis.rpRequestCache = rpRequestCache
+globalThis.rpResponseCache = rpResponseCache
